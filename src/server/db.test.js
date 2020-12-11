a1557e9acb67353a3a6cc24fcc38bb3cb7ea9dd6
snowdot <co.pascu@gmail.com>
2020-12-11 23:31:39 +0200

    added helper functions to have clean code

--- src/server/db.test.js
+++ src/server/db.test.js
@@ -1,18 +1,9 @@
-import { MongoClient } from 'mongodb';
 import { expect } from 'chai';
 import { getUserByUsername } from './db';
+import { getDatabaseData, setDatabaseData, resetDatabase } from './test-helpers';
 
 describe('getUserByUsername', () => {
     it('get the correct user from the database given a username', async () => {
-        const client = await MongoClient.connect(
-            `mongodb://localhost:27017/TEST_DB`,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            },
-        );
-        const db = client.db('TEST_DB');
-
         const fakeData = [{
             id: '123',
             username: 'abc',
@@ -23,11 +14,11 @@ describe('getUserByUsername', () => {
             email: 'wrong@wrong.com',
         }];
 
-        await db.collection('users').insertMany(fakeData);
+        await setDatabaseData('users', fakeData);
+
         const actual = await getUserByUsername('abc');
-        const finalDBState = await db.collection('users').find().toArray();
-        await db.dropDatabase();
-        client.close();
+        const finalDBState = await getDatabaseData('users');
+        await resetDatabase();
 
         const expected = {
             id: '123',
