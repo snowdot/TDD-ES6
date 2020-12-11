2533f1b2f02f17a56bebcbbcbb0eea40e14e3571
snowdot <co.pascu@gmail.com>
2020-12-11 23:34:31 +0200

    added an afterEach mocha hook

--- src/server/db.test.js
+++ src/server/db.test.js
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
+import { async } from 'regenerator-runtime';
 import { getUserByUsername } from './db';
 import { getDatabaseData, setDatabaseData, resetDatabase } from './test-helpers';
 
 describe('getUserByUsername', () => {
+    afterEach('reset the database', async() => {
+        await resetDatabase();
+    });
+
     it('get the correct user from the database given a username', async () => {
         const fakeData = [{
             id: '123',
@@ -18,8 +23,7 @@ describe('getUserByUsername', () => {
 
         const actual = await getUserByUsername('abc');
         const finalDBState = await getDatabaseData('users');
-        await resetDatabase();
-
+        
         const expected = {
             id: '123',
             username: 'abc',
